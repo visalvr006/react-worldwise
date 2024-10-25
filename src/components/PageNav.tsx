7749412e78037d3bcfe09337a5f5932c59d55d60
Visal VR <visal.v@thinkpalm.com>
2024-10-25 11:06:23 +0530

    ADD: Added basic components.

---
+++ src/components/PageNav.tsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.module.css";
+
+function PageNav() {
+  return <nav className={styles.nav}>Page nav</nav>;
+}
+
+export default PageNav;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM, { Container } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as Container).render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>
+);
